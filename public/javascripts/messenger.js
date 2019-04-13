7af77ebb64d2e4286cf46c41b7704e0e7ddf7ba7
Morgan Parry <morgan.parry@cloud-iq.com>
2019-04-13 16:54:49 +0100

    refactor JS code in to messenger.js

--- public/javascripts/messenger.js
+++ public/javascripts/messenger.js
@@ -1,5 +1,28 @@
-function checkLoginState() {
-    FB.getLoginStatus(function(response) {
-      statusChangeCallback(response);
-    });
-  }
+/**
+ *  This File initializes the Facebook SDK
+ *  We then check on page load if the user is logged in.. if so the send to messneger button will display!
+ * 
+ * This is where we can display the login button if the user is not logged in! J
+ * ust need to dynamically append it to the DOM
+ */
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'))
+    window.fbAsyncInit = function() {
+      FB.init({
+      appId      : '1251666588315299', // Replace with your APP ID
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v3.2'
+      })
+  
+      FB.AppEvents.logPageView()
+      FB.getLoginStatus(function(response) {
+      console.log(response)
+      });
+      }
