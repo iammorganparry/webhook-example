5440ee576c15d38675180a10687c0c9bc83649ad
Morgan Parry <morgan.parry@cloud-iq.com>
2019-04-13 09:39:39 +0100

    Add axios for post request

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,7 +6,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Imports dependencies and set up http server
 const
   // request = require('request'),
-  axios = require('axios')
+  axios = require('axios'),
   express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()); // creates express http server
