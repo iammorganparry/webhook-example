d12f9052f8e05835ebe918f1f4e2833e56c84e18
Morgan Parry <morgan.parry@cloud-iq.com>
2019-04-13 20:10:14 +0100

    add product page

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var productRouter = require('./routes/product');
 const webhookRouter = require('./routes/webhook')
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/product', productRouter);
 app.use('/webhook', webhookRouter);
 
 // catch 404 and forward to error handler
